a736749c6b698756fb0e52b8ce8400acaeb4ab9d
James <jamesrwyatt35@gmail.com>
2024-05-09 15:45:26 -0400

    init commit

---
+++ src/main/java/org/jwctech/stripespringapi/StripeSpringApiApplication.java
@@ -0,0 +1,13 @@
+package org.jwctech.stripespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripeSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jwctech/stripespringapi/StripeSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jwctech.stripespringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
